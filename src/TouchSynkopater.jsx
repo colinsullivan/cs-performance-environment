67ac44ddf023c638047dd2909c04f9481054e11b
Colin Sullivan <colin@colin-sullivan.net>
2019-02-02 16:48:10 -0800

    more duration options

--- src/TouchSynkopater.jsx
+++ src/TouchSynkopater.jsx
@@ -44,7 +44,21 @@ class TouchSynkopater extends React.Component {
     ];
 
     const factorOptions = [
+      {value: 4 + 3/4, label: "4 3/4"},
+      {value: 4 + 1/2, label: "4 1/2"},
+      {value: 4 + 2/3, label: "4 2/3"},
+      {value: 4 + 1/3, label: "4 1/3"},
+      {value: 4 + 1/8, label: "4 1/8"},
+      {value: 4 + 1/16, label: "4 1/16"},
+      {value: 4 + 1/32, label: "4 1/32"},
       {value: 4, label: "4"},
+      {value: 3 + 3/4, label: "3 3/4"},
+      {value: 3 + 1/2, label: "3 1/2"},
+      {value: 3 + 2/3, label: "3 2/3"},
+      {value: 3 + 1/3, label: "3 1/3"},
+      {value: 3 + 1/8, label: "3 1/8"},
+      {value: 3 + 1/16, label: "3 1/16"},
+      {value: 3 + 1/32, label: "3 1/32"},
       {value: 3, label: "3"},
       {value: 2 + 3/4, label: "2 3/4"},
       {value: 2 + 1/2, label: "2 1/2"},
@@ -62,6 +76,13 @@ class TouchSynkopater extends React.Component {
       {value: 1 + 1/16, label: "1 1/16"},
       {value: 1 + 1/32, label: "1 1/32"},
       {value: 1, label: "1"},
+      {value: 3/4, label: "3/4"},
+      {value: 1/2, label: "1/2"},
+      {value: 2/3, label: "2/3"},
+      {value: 1/3, label: "1/3"},
+      {value: 1/8, label: "1/8"},
+      {value: 1/16, label: "1/16"},
+      {value: 1/32, label: "1/32"},
     ];
 
     return (
