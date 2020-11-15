12efe37baebb650f34dde3ffc9e129bbf3d4eff7
Colin Sullivan <colin@colin-sullivan.net>
2020-11-15 13:18:47 -0800

    quit supercollider when app is quit

--- src/main/index.js
+++ src/main/index.js
@@ -8,21 +8,22 @@
  *  @license    Licensed under the GPLv3 license.
  **/
 
-import path from 'path';
-import electron from 'electron';
-import { createStore, applyMiddleware } from "redux"
-import express from 'express';
-import expressWebsocket from 'express-ws';
-import SCRedux from 'supercollider-redux';
+import path from "path";
+import electron from "electron";
+import { createStore, applyMiddleware } from "redux";
+import express from "express";
+import expressWebsocket from "express-ws";
+import SCRedux from "supercollider-redux";
 
-import WebsocketServerDispatcher from './WebsocketServerDispatcher';
+import WebsocketServerDispatcher from "./WebsocketServerDispatcher";
 
-import rootReducer, {create_default_state} from '../common/reducers';
-import { PORT } from '../common/constants';
+import rootReducer, { create_default_state } from "../common/reducers";
+import { PORT } from "../common/constants";
 
 //const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
+
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
@@ -33,72 +34,23 @@ const BrowserWindow = electron.BrowserWindow;
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1440, height: 900})
-
-  // and load the index.html of the app.
-  if (process.env.NODE_ENV === "development") {
-    console.log("development");
-    mainWindow.loadURL("http://localhost:3000/laptop");
-  } else {
-    mainWindow.loadURL("http://localhost:3001/laptop");
-  }
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
 const wsServerDispatcher = new WebsocketServerDispatcher();
 console.log("Creating store...");
-var loggerMiddleware = store => next => action => {
-  console.log('will dispatch', action)
+var loggerMiddleware = (store) => (next) => (action) => {
+  console.log("will dispatch", action);
 
   // Call the next dispatch method in the middleware chain.
-  const returnValue = next(action)
+  const returnValue = next(action);
 
-  console.log('state after dispatch', store.getState())
+  console.log("state after dispatch", store.getState());
 
   // This will likely be the action itself, unless
   // a middleware further in chain changed it.
-  return returnValue
+  return returnValue;
 };
 var middleware = [wsServerDispatcher.middleware];
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   middleware.push(loggerMiddleware);
 }
 
@@ -111,19 +63,22 @@ var store = createStore(
 console.log("Initializing SCRedux");
 const scReduxController = new SCRedux.SCReduxController(store, {
   interpretOnLangBoot: `
-MIDIClient.init;
-MIDIIn.connectAll;
-s.options.inDevice = "JackRouter";
-s.options.outDevice = "JackRouter";
+API.mountDuplexOSC();
+s.options.inDevice = "UltraLite + BlackHole";
+s.options.outDevice = "UltraLite + BlackHole";
 s.options.numOutputBusChannels = 48;
 s.options.numInputBusChannels = 48;
-s.options.memSize = 8192 * 2 * 2 * 2;
+s.options.memSize = 1024000;
 s.options.blockSize = 8;
 
 s.waitForBoot({
   var m = s.meter(),
     mBounds,
     performanceEnvironment;
+
+  MIDIClient.init();
+  MIDIIn.connectAll();
+
   // move level meter to bottom right of screen
   mBounds = m.window.bounds;
   mBounds.left = 1440;
@@ -131,46 +86,48 @@ s.waitForBoot({
   m.window.setTopLeftBounds(mBounds);
 
   // debugging
-  //s.plotTree();
-
+  s.plotTree();
+  
   performanceEnvironment = CSPerformanceEnvironment.new();
-});`
+});
+
+`,
 });
 
 const startServer = () => {
   const server = express();
   expressWebsocket(server);
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     server.use(function (req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "X-Requested-With");
       next();
     });
-    server.use(express.static('public'));
+    server.use(express.static("public"));
   } else {
-    server.use(express.static(path.join(__dirname, '..')));
+    server.use(express.static(path.join(__dirname, "..")));
   }
-  server.get('/getState', function (req, res) {
+  server.get("/getState", function (req, res) {
     res.json(store.getState());
   });
-  server.ws('/:clientId', function (ws, req) {
+  server.ws("/:clientId", function (ws, req) {
     const clientId = req.params.clientId;
     console.log(`client ${clientId} connected.`);
-    ws.on('message', function (msg) {
+    ws.on("message", function (msg) {
       const action = JSON.parse(msg);
       store.dispatch(action);
     });
-    ws.on('close', function () {
+    ws.on("close", function () {
       wsServerDispatcher.removeClient(clientId);
     });
     wsServerDispatcher.addClient(clientId, ws);
   });
-  if (process.env.NODE_ENV !== 'development') {
-    server.get('/', function (req, res) {
+  if (process.env.NODE_ENV !== "development") {
+    server.get("/", function (req, res) {
       res.sendFile(path.join(`${__dirname}/../index.html`));
     });
-    server.get('/laptop', function (req, res) {
+    server.get("/laptop", function (req, res) {
       res.sendFile(path.join(`${__dirname}/../index.html`));
     });
   }
@@ -180,8 +137,64 @@ const startServer = () => {
   console.log(`Listening on port ${PORT}.`);
 };
 
-scReduxController.boot().then(startServer).catch(function (err) {
-  console.log("error while starting up...");
-  throw err;
+const quit = () =>
+  scReduxController.quit().then(() => {
+    app.quit();
+  });
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({ width: 1440, height: 900 });
+
+  // and load the index.html of the app.
+  if (process.env.NODE_ENV === "development") {
+    console.log("development");
+    mainWindow.loadURL("http://localhost:3000/laptop");
+  } else {
+    mainWindow.loadURL("http://localhost:3001/laptop");
+  }
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on("closed", function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    quit();
+  }
 });
 
+app.on("will-quit", quit);
+
+app.on("activate", function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+scReduxController
+  .boot()
+  .then(startServer)
+  .catch(function (err) {
+    console.log("error while starting up...");
+    console.log(err);
+    quit();
+  });
