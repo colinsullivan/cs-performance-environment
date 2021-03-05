00b0547bc7f8f85c0524c13298caa2b7f1615440
Colin Sullivan <colin@colin-sullivan.net>
2021-03-04 19:13:49 -0800

    Removes Electron, was not using it.  Build still does not work.

--- src/main/index.js
+++ src/main/index.js
@@ -10,7 +10,7 @@
 
 import path from "path";
 import fs from "fs";
-import electron from "electron";
+
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import express from "express";
@@ -22,20 +22,6 @@ import WebsocketServerDispatcher from "./WebsocketServerDispatcher";
 import rootReducer, { create_default_state } from "../common/reducers";
 import { PORT } from "../common/constants";
 import { rehydrate_state } from "../common/actions";
-
-//const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
-//const path = require('path');
-//const url = require('url');
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
 
 const wsServerDispatcher = new WebsocketServerDispatcher();
 console.log("Creating store...");
@@ -68,18 +54,16 @@ const scReduxController = new SCRedux.SCReduxController(store, {
   interpretOnLangBoot: fs.readFileSync(path.join(__dirname, "sclang_init.sc")),
 });
 
-const quit = () =>
+const quit = () => {
+  console.log("Stopping...");
   scReduxController.quit().then(() => {
-    app.quit();
+    console.log("Bye!");
+    process.exit();
   });
+}
 
-const load = () => {
-  if (process.env.NODE_ENV === "development") {
-    mainWindow.loadURL("http://localhost:3000/laptop");
-  } else {
-    mainWindow.loadURL("http://localhost:3001/laptop");
-  }
-};
+
+process.on("SIGINT", quit);
 
 const startServer = () => {
   const server = express();
@@ -125,49 +109,7 @@ const startServer = () => {
   console.log(`Starting to listen on port ${PORT}...`);
   server.listen(PORT);
   console.log(`Listening on port ${PORT}.`);
-  load();
 };
-
-function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 1440, height: 900 });
-
-  if (process.env.NODE_ENV === "development") {
-    mainWindow.webContents.openDevTools();
-  }
-
-  // Emitted when the window is closed.
-  mainWindow.on("closed", function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
-
-// Quit when all windows are closed.
-app.on("window-all-closed", function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    quit();
-  }
-});
-
-app.on("will-quit", quit);
-
-app.on("activate", function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
 
 scReduxController
   .boot()
