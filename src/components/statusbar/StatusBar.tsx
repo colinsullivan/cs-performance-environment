4b7e88d8f9888e2dda6f946705913b9f74f4acc5
Colin Sullivan <colin@colin-sullivan.net>
2022-03-20 15:15:39 -0700

    Adds display of tempo in top bar

--- src/components/statusbar/StatusBar.tsx
+++ src/components/statusbar/StatusBar.tsx
@@ -2,10 +2,12 @@ import ConnectionStatus from "components/statusbar/ConnectionStatus";
 import OctatrackStatus from "components/statusbar/OctatrackStatus";
 
 import "./StatusBar.scss";
+import TempoDisplay from "./TempoDisplay";
 
 const StatusBar = () => {
   return (
     <div className="status-bar">
+      <TempoDisplay />
       <OctatrackStatus />
       <ConnectionStatus />
     </div>
