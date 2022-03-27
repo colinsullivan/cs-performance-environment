14e49f0486890850825a38e4ce45f006bf3ab0db
Colin Sullivan <colin@colin-sullivan.net>
2022-03-27 00:47:51 -0700

    State changes are sent to a Max dict in full

--- src/main/MaxDispatcher.ts
+++ src/main/MaxDispatcher.ts
@@ -1,6 +1,8 @@
 import max from "max-api"
+import { Store } from "redux";
 
 class MaxDispatcher {
+  store: Store | undefined;
   constructor() {
     //super();
 
@@ -11,8 +13,37 @@ class MaxDispatcher {
     });
   }
 
-  setStore() {
-    console.log("hello");
+  getStore () {
+    if (!this.store) {
+      throw new Error("Store not ready");
+    }
+    return this.store;
+  }
+
+  private async handleStateChange () {
+    const state = this.getStore().getState();
+
+    try {
+      await max.setDict("cs/state", state);
+    } catch (e) {
+      console.log("Error setting cs/state");
+      console.log("e");
+      console.log(e);
+    }
+    try {
+      await max.outlet("cs/state_changed");
+      console.log("debug");
+    } catch (e) {
+      console.log("Error sending change notification");
+      console.log("e");
+      console.log(e);
+    }
+  }
+
+  setStore(inStore: Store) {
+    this.store = inStore;
+
+    this.store.subscribe(() => this.handleStateChange());
   }
 }
 
