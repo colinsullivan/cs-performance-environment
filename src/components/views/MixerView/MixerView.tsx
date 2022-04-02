83b7d1c838fdf9074c734d7caf81c595145dbad7
Colin Sullivan <colin@colin-sullivan.net>
2022-04-01 18:55:07 -0700

    basic layout of channels

--- src/components/views/MixerView/MixerView.tsx
+++ src/components/views/MixerView/MixerView.tsx
@@ -1,14 +1,25 @@
 import { getAbletonTracks } from "common/selectors";
 import React from "react";
+import {createUseStyles} from "react-jss";
 import { useSelector } from "react-redux";
 import OverlayView from "../OverlayView";
 import MixerTrack from "./MixerTrack";
 
+const useStyles = createUseStyles({
+  mixer: {
+    display: "flex",
+    flexDirection: "row",
+    height: "100%",
+    flexWrap: "wrap",
+  }
+});
+
 const MixerView = () => {
   const tracks = useSelector(getAbletonTracks);
+  const styles = useStyles();
   return (
     <OverlayView>
-      <div>
+      <div className={styles.mixer}>
         {tracks.map(t => <MixerTrack key={t.name} track={t} />)}
       </div>
     </OverlayView>
